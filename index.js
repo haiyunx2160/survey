155432f7913d39e31165f8657d6c9b062193f5a5
Aaron <haiyunx2160@gmail.com>
2020-05-05 19:41:52 -0400

    fix index

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ require('./routes/billingRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
-}
 
-const path = require('path');
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-});
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
